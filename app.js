8e5390984f6e4cf7db9a90fb83cd05e6ea735812
Dmitri Gaponenko <54118154+dmitri-gaponenko@users.noreply.github.com>
2024-02-05 16:45:14 +0300

    init: implement url-shortener-microservice

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8081;
+const cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));
+
+app.use(express.urlencoded());
+
+/* url object example:
+  {
+    long: 'https://www.radiorecord.ru/',
+    short: 1,
+  }
+ */
+const urls = [];
+
+app.post('/api/shorturl', (req, res) => {
+  console.log('body is', req.body);
+  const urlFromBody = req.body.url;
+
+  if (!urlFromBody) {
+    return res.json({"error": "Invalid URL"});
+  }
+
+  //TODO: check URL
+
+  let urlObj = urls.find((url) => url.long === urlFromBody);
+  if (!urlObj) {
+    urlObj = {
+      long: urlFromBody,
+      short: urls.length === 0 ? 1 : urls[urls.length - 1].short + 1,
+    };
+    urls.push(urlObj);
+  }
+
+  const result = {
+    original_url: urlObj.long,
+    short_url: urlObj.short,
+  };
+
+  return res.json(result);
+});
+
+app.get('/api/shorturl/:url', (req, res) => {
+  const shortUrlFromParams = req.params.url;
+
+  if (!Number(shortUrlFromParams) || Number(shortUrlFromParams) < 1) {
+    return res.json({"error": "Wrong format"});
+  }
+
+  const urlObj = urls.find((url) => url.short === Number(shortUrlFromParams));
+
+  if (!urlObj) {
+    return res.json({"error": "No short URL found for the given input"});
+  }
+
+  return res.redirect(urlObj.long);
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
